2d2d25736365083b864f06326e19264cc62af8cb
Jesper <jetolu@gmail.com>
2017-11-15 16:00:35 +0100

    Added wrongculator stuff to wrongculator

--- Wrongculator/src/com/company/Main.java
+++ Wrongculator/src/com/company/Main.java
@@ -12,7 +12,17 @@ public class Main {
         int i = sc.nextInt();
         System.out.print("Write another number:");
         int j = sc.nextInt();
-        System.out.println("The result is: " + wrongculator.add(i, j));
+        System.out.println("Add(1), Subtract(2), Divide(3), Multiply(4)");
+        int k = sc.nextInt();
+        if (k == 1) {
+            System.out.println("The result is: " + wrongculator.add(i, j));
+        } else if (k == 2) {
+            System.out.println("The result is: " + wrongculator.sub(i, j));
+        } else if (k == 3) {
+            System.out.println("The result is: " + wrongculator.div(i, j));
+        } else if (k == 4) {
+            System.out.println("The result is: " + wrongculator.mul(i, j));
+        }
 
     }
 }
--- Wrongculator/src/com/company/Wrongculator.java
+++ Wrongculator/src/com/company/Wrongculator.java
@@ -3,15 +3,15 @@ package com.company;
 public class Wrongculator {
 
     public int add(int a, int b) {
-        return a + b;
+        return a + b - 3;
     }
     public int sub(int a, int b) {
-        return a - b;
+        return a - b + 18;
     }
     public int mul(int a, int b) {
-        return a * b;
+        return a * b /7;
     }
     public int div(int a, int b) {
-        return a / b;
+        return a / b * 13;
     }
 }
