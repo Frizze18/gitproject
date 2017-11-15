a9b1cf4771d127c722826b2a13330780b7027a11
Jesper <jetolu@gmail.com>
2017-11-15 14:37:33 +0100

    Add add function and main method to Wrongculator

---
+++ Wrongculator/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Wrongculator wrongculator = new Wrongculator();
+        int x = wrongculator.add(3,4);
+        System.out.println(x);
+
+    }
+}
---
+++ Wrongculator/src/com/company/Wrongculator.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Wrongculator {
+
+    public int add (int a, int b) {
+        return a+b;
+    }
+}
